2c007951648ad1831bceb7881d9dd7ed652fcc48
Kedar Mhaswade <kedar@jymob.com>
2014-08-09 18:30:42 -0700

    add scripts to get rid of maven exec plugin issues

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+MYDIR=$(dirname $0)
+CALIPER_JAR=~/.m2/repository/com/google/caliper/caliper/1.0-beta-SNAPSHOT/caliper-1.0-beta-SNAPSHOT-all.jar
+if [[ ! -f ${CALIPER_JAR} ]]; then
+  echo "Follow the instructions; build caliper first in a separate folder"
+  exit 1
+fi
+java -server -Xmx2g -Xms2g -cp ${CALIPER_JAR}:${MYDIR}/target/classes com.google.caliper.runner.CaliperMain com.lithium.bm.StringCmpBenchmark $*
